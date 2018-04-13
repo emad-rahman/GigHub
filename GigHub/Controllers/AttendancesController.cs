28923f9abff45ec70c189b6cfd3487285372c475
guardianz95 <emadpoke@gmail.com>
2018-04-13 16:35:39 -0700

    added api to add attendee to a gig

---
+++ GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Web.Http;
+using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _db;
+
+        public AttendancesController()
+        {
+            _db = new ApplicationDbContext();
+        }
+       
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_db.Attendances.Any(a => a.AttendeeId == userId && a.GigId == dto.GigId))
+            {
+                return BadRequest("The attendance alreay exists.");
+            }
+
+            var attendance = new Attendance()
+            {
+                GigId = dto.GigId,
+                AttendeeId = userId
+            };
+
+            _db.Attendances.Add(attendance);
+            _db.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
