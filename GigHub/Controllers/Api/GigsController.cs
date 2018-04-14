1fd988f7792d5ee2eaae9c07e28b84a4da76efc3
guardianz95 <emadpoke@gmail.com>
2018-04-14 15:44:44 -0700

    Cancel gig

---
+++ GigHub/Controllers/Api/GigsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Web.Http;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GigHub.Controllers.Api
+{
+    [Authorize]
+    public class GigsController : ApiController
+    {
+        private readonly ApplicationDbContext _db;
+
+        public GigsController()
+        {
+            _db = new ApplicationDbContext();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Cancel(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var gig = _db.Gigs
+                .Where(g => g.Id == id)
+                .Single(g => g.ArtistId == userId);
+
+            gig.IsCanceled = true;
+            _db.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,10 +1,10 @@
-﻿using GigHub.Models;
-using GigHub.ViewModels;
-using Microsoft.AspNet.Identity;
-using System;
+﻿using System;
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
+using GigHub.Models;
+using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
 
 namespace GigHub.Controllers
 {
@@ -22,9 +22,10 @@ namespace GigHub.Controllers
         {
             var viewModel = new GigFormViewModel
             {
-                Genres = _db.Genres.ToList()
+                Genres = _db.Genres.ToList(),
+                Heading = "Add a gig"
             };
-            return View(viewModel);
+            return View("GigForm", viewModel);
         }
 
         [HttpPost]
@@ -35,8 +36,9 @@ namespace GigHub.Controllers
             if (!ModelState.IsValid)
             {
                 viewModel.Genres = _db.Genres.ToList();
-                return View(viewModel);
+                return View("GigForm", viewModel);
             }
+
             var gig = new Gig
             {
                 ArtistId = User.Identity.GetUserId(),
@@ -51,19 +53,65 @@ namespace GigHub.Controllers
             return RedirectToAction("Mine", "Gigs");
         }
 
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update(GigFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _db.Genres.ToList();
+                return View("GigForm", viewModel);
+            }
+
+            var userId = User.Identity.GetUserId();
+            var gig = _db.Gigs
+                .Where(g => g.Id == viewModel.Id)
+                .Single(g => g.ArtistId == userId);
+            gig.Venue = viewModel.Venue;
+            gig.DateTime = viewModel.GetDateTime();
+            gig.GenreId = viewModel.Genre;
+         
+            _db.SaveChanges();
+
+            return RedirectToAction("Mine", "Gigs");
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var gig = _db.Gigs
+                .Where(g => g.Id == id)
+                .Single(g => g.ArtistId == userId);
+
+            var viewModel = new GigFormViewModel
+            {
+                Heading = "Edit gig",
+                Id = gig.Id,
+                Genres = _db.Genres.ToList(),
+                Date = gig.DateTime.ToString("d MMM yyyy"),
+                Time = gig.DateTime.ToString("HH:mm"),
+                Genre = gig.GenreId,
+                Venue = gig.Venue                
+            };
+
+            return View("GigForm", viewModel);
+        }
+
         [Authorize]
         [HttpGet]
         public ActionResult Attending()
         {
             var userId = User.Identity.GetUserId();
-            var gigs = _db.Attendances.
-                Where(a => a.AttendeeId == userId)
+            var gigs = _db.Attendances.Where(a => a.AttendeeId == userId)
                 .Select(a => a.Gig)
                 .Include(a => a.Artist)
                 .Include(g => g.Genre)
                 .ToList();
 
-            var viewModel = new GigsViewModel()
+            var viewModel = new GigsViewModel
             {
                 UpcomingGigs = gigs,
                 ShowActions = User.Identity.IsAuthenticated,
